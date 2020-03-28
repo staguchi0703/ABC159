60931e843c3a7106d7e4b6b0632231abd86de0dd
sho Taguchi <s.taguchi0703@gmail.com>
2020-03-28 17:50:46 +0900

    try

--- A/resolve.py
+++ A/resolve.py
@@ -2,6 +2,10 @@ def resolve():
     '''
     code here
     '''
+    N, M = [int(item) for item in input().split()]
+    res = N*(N-1)//2 + M*(M-1)//2
+    print(res)
+
 
 
 if __name__ == "__main__":
--- B/resolve.py
+++ B/resolve.py
@@ -2,7 +2,23 @@ def resolve():
     '''
     code here
     '''
+    S = input()
+    N = len(S)
 
+
+    def chk_kaibun(S):
+        is_kaibun = False
+        center_index = (len(S)-1)//2
+        if S[:center_index] == S[center_index:-1:-1]:
+            is_kaibun = True
+        return is_kaibun
+
+    is_front = False
+    front_string = S[:]
+
+    if is_kaibun and is_flag:
+        is_ans = True
+    print('Yes') if is_ans else print('No')
 
 if __name__ == "__main__":
     resolve()
